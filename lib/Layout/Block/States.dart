c88e3f2909b148f761b353cc4bca5e14daf11d2a
AhmedAlkori <dev.alkori87@example.com>
2023-09-22 23:48:54 +0300

    Sport_Screen Added By Ahmed

--- lib/Layout/Block/States.dart
+++ lib/Layout/Block/States.dart
@@ -9,3 +9,7 @@ class NewsChangeModeState extends NewsState{}
 class NewsGetBusinessLoadingState extends NewsState{}
 class NewsGetBusinessSuccessState extends NewsState{}
 class NewsGetBusinessErrorState extends NewsState{}
+
+class NewsGetSportsLoadingState extends NewsState{}
+class NewsGetSportsSuccessState extends NewsState{}
+class NewsGetSportsErrorState extends NewsState{}
